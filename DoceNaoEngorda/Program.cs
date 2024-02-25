1f2d010b25a3761f78e87a135f37f8804f1b8ac5
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2024-02-25 17:57:47 -0300

    Implementado método para criar e listar os produtos

--- DoceNaoEngorda/Program.cs
+++ DoceNaoEngorda/Program.cs
@@ -1,23 +1,12 @@
-﻿internal class Program
+﻿using DoceNaoEngorda;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int escolhaUsurario = Menu();
-
-        _ = escolhaUsurario switch
-        {
-            1 => "Novo",
-            2 => "Listar Produtos",
-            3 => "Remover Produtos",
-            4 => "Entrada Estoque",
-            5 => "Saída Estoque",
-            _ => "Sair"
-        };
-    }
-
-    private static int Menu()
-    {
         int escolhaMenu;
+        Sobremesa[] listaDeDoces = [];
+
         do
         {
             Console.WriteLine("\n>>---> Doce Não Engorda! <---<<\n\n" +
@@ -29,14 +18,36 @@
             "  [0] Sair\n");
 
             Console.Write("\nDigite sua escolha: ");
-            escolhaMenu = ValidaEscolhaDoUsario();
+            escolhaMenu = ValidaEscolhaDoUsuario();
+
+            switch (escolhaMenu)
+            {
+                case 1:
+                    Novo(ref listaDeDoces);
+                    break;
+                case 2:
+                    ListarProdutos(ref listaDeDoces);
+                    break;
+                case 3:
+                    RemoverProdutos();
+                    break;
+                case 4:
+                    EntradaEstoque();
+                    break;
+                case 5:
+                    SaidaEstoque();
+                    break;
+                case 0:
+                    Sair();
+                    break;
+            }
 
         } while (escolhaMenu != 0);
 
-        return escolhaMenu;
+
     }
 
-    private static int ValidaEscolhaDoUsario()
+    private static int ValidaEscolhaDoUsuario()
     {
         if (!int.TryParse(Console.ReadLine(), out int escolhaMenu))
         {
@@ -45,5 +56,76 @@
         }
 
         return escolhaMenu;
+    }
+
+    private static void Novo(ref Sobremesa[] listaDeDoces)
+    {
+        Sobremesa doce = new();
+
+        Console.Write("  Código: ");
+        doce.Codigo = int.Parse(Console.ReadLine());
+
+        Console.Write("  Nome da Sobremesa: ");
+        doce.Nome = Console.ReadLine();
+
+        Console.Write("  Data da Fabricação: ");
+        doce.DataDaFabricao = DateTime.Parse(Console.ReadLine());
+
+        Console.Write($"  Qual o peso? ");
+        doce.Peso = double.Parse(Console.ReadLine());
+
+        Console.Write("  Quanto custa: ");
+        doce.Preco = decimal.Parse(Console.ReadLine());
+
+        listaDeDoces = AdicionaNovoDoceNaLista(listaDeDoces, doce);
+    }
+
+    private static Sobremesa[] AdicionaNovoDoceNaLista(Sobremesa[] listaDeDoces, Sobremesa doce)
+    {
+        if (listaDeDoces != null)
+        {
+            Sobremesa[] listaTemporariaDeDoces = new Sobremesa[listaDeDoces.Length + 1];
+            for (int i = 0; i < listaDeDoces.Length; i++)
+            {
+                listaTemporariaDeDoces[i] = listaDeDoces[i];
+            }
+            listaTemporariaDeDoces[listaDeDoces.Length] = doce;
+            listaDeDoces = listaTemporariaDeDoces;
+        }
+
+        return listaDeDoces;
+    }
+
+    private static void ListarProdutos(ref Sobremesa[] listaDeDoces)
+    {
+        for (int i = 0; i < listaDeDoces.Length; i++)
+        {
+            Console.WriteLine($"\n  Cod: {listaDeDoces[i].Codigo}.\n" +
+                $"  Sobremesa: {listaDeDoces[i].Nome}.\n" +
+                $"  Data de Fabricação: {listaDeDoces[i].DataDaFabricao.ToShortDateString()}.\n" +
+                $"  Preço: {listaDeDoces[i].Preco}.\n" +
+                $"  Peso: {listaDeDoces[i].Peso}.\n" +
+                $"  Estoque: {listaDeDoces[i].QuantidadeEmEstoque}.");
+        }
+    }
+
+    private static void RemoverProdutos()
+    {
+        Console.WriteLine("RemoverProdutos");
+    }
+
+    private static void EntradaEstoque()
+    {
+        Console.WriteLine("EntradaEstoque");
+    }
+
+    private static void SaidaEstoque()
+    {
+        Console.WriteLine("SaidaEstoque");
+    }
+
+    private static void Sair()
+    {
+        Console.WriteLine("Sair");
     }
 }
---
+++ DoceNaoEngorda/Sobremesa.cs
@@ -0,0 +1,14 @@
+﻿namespace DoceNaoEngorda
+{
+    public class Sobremesa
+    {
+        public int Codigo { get; set; }
+        public string? Nome { get; set; }
+        public DateTime DataDaFabricao { get; set; }
+        public double Peso { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeEmEstoque { get; set; } = 0;
+
+
+    }
+}
