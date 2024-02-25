171b2a5d579ff8bc6e29cd8591598cbd4763432d
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2024-02-25 10:27:35 -0300

    Initial commit

---
+++ DoceNaoEngorda/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int escolhaUsurario = Menu();
+
+        _ = escolhaUsurario switch
+        {
+            1 => "Novo",
+            2 => "Listar Produtos",
+            3 => "Remover Produtos",
+            4 => "Entrada Estoque",
+            5 => "Saída Estoque",
+            _ => "Sair"
+        };
+    }
+
+    private static int Menu()
+    {
+        int escolhaMenu;
+        do
+        {
+            Console.WriteLine("\n>>---> Doce Não Engorda! <---<<\n\n" +
+            "  [1] Novo\n" +
+            "  [2] Listar Produtos\n" +
+            "  [3] Remover Produtos\n" +
+            "  [4] Entrada Estoque\n" +
+            "  [5] Saída Estoque\n" +
+            "  [0] Sair\n");
+
+            Console.Write("\nDigite sua escolha: ");
+            escolhaMenu = ValidaEscolhaDoUsario();
+
+        } while (escolhaMenu != 0);
+
+        return escolhaMenu;
+    }
+
+    private static int ValidaEscolhaDoUsario()
+    {
+        if (!int.TryParse(Console.ReadLine(), out int escolhaMenu))
+        {
+            Console.WriteLine("\nERRO: Você precisa digitar uma das opções!");
+            escolhaMenu = 6;
+        }
+
+        return escolhaMenu;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DoceNaoEngorda
+ Projeto Controle de Estoque da disciplina Algoritmos II da graduação em Análise e Desenvolvimento de Sistemas do Senac
